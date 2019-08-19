d1260ef0729a47dbc057631e1b4ebf4beade51cf
Vincent Fournier <v.fournier11@gmail.com>
2019-08-20 12:42:12 +1400

    fix tests

--- test.js
+++ test.js
@@ -21,8 +21,8 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+      t.notEqual(body.indexOf("<title>Vincent Fournier webfolio</title>"), -1);
+      t.notEqual(body.indexOf("Vincent Fournier - ntfournier"), -1);
     });
   });
 });
